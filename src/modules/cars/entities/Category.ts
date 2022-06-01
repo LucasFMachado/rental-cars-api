9aca791f0245bf311cff4664e2d7da00a9f59c15
Lucas Machado <lucas.fmachado19@gmail.com>
2022-06-01 09:50:56 -0300

    Adição de ORM e database ao projeto

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,15 +1,25 @@
 import { v4 as uuid } from 'uuid';
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
 
+@Entity("categories")
 class Category {
+
+  @PrimaryColumn()
   id: string;
+  
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
     if (!this.id) {
       this.id = uuid();
-      this.created_at = new Date();
+      // this.created_at = new Date();
     }
   }
 }
--- src/modules/cars/useCases/createCategory/CreateCategoryService.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryService.ts
@@ -8,8 +8,8 @@ interface ICreateCategoryRequest {
 class CreateCategoryService {
   constructor(private categoriesRepository: ICategoriesRepository) {}
 
-  execute({ name, description }: ICreateCategoryRequest): void {
-    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+  async execute({ name, description }: ICreateCategoryRequest): Promise<void> {
+    const categoryAlreadyExists = await this.categoriesRepository.findByName(name);
 
     if (categoryAlreadyExists) {
       throw new Error('Category already exists.');
